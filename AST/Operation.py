a05a00ee47b1a4a0210d1602ae26bacdda18dd61
Fabian Wunsch <fabian@uriah.heep.sax.de>
2022-06-02 23:18:13 +0200

    Extended Operation

--- AST/Operation.py
+++ AST/Operation.py
@@ -4,7 +4,8 @@ import numpy as np
 from typing import Optional, Any
 
 from .ASTNode import ASTNode, Signals
-from .Operand import Operand, ConstantOperand
+from .Operand import Operand, ConstantOperand, SignalOperand
+from .Signal import Signal, ConcreteSignal
 
 
 class NumericOperator(Enum):
@@ -20,18 +21,48 @@ class NumericOperator(Enum):
     OR = "OR"
     XOR = "XOR"
 
+    def calculate(self, left: np.int32, right: np.int32) -> np.int32:
+        if self == NumericOperator.ADD:
+            return left + right
+        elif self == NumericOperator.SUBTRACT:
+            return left - right
+        elif self == NumericOperator.MULTIPLY:
+            return left * right
+        elif self == NumericOperator.DIVIDE:
+            return left // right
+        elif self == NumericOperator.MODULO:
+            return left % right
+        elif self == NumericOperator.EXPONENT:
+            return left**right
+        elif self == NumericOperator.LEFT_BIT_SHIFT:
+            return left << right
+        elif self == NumericOperator.RIGHT_BIT_SHIFT:
+            return left >> right
+        elif self == NumericOperator.AND:
+            return left & right
+        elif self == NumericOperator.OR:
+            return left | right
+        elif self == NumericOperator.XOR:
+            return left ^ right
+        raise NotImplementedError(f"Unknown operator {self}")
+
 
 class Operation(ASTNode):
     def __init__(
         self,
         operation: NumericOperator,
+        result: SignalOperand,
         left: Optional[Operand] = None,
         right: Optional[Operand] = None,
     ):
         super().__init__("Operation")
         self.operation: NumericOperator = operation
-        self.left: Operand = left if left else ConstantOperand(np.int32(0))
-        self.right: Operand = right if right else ConstantOperand(np.int32(0))
+        self._left: Operand
+        self._right: Operand
+        self._result: SignalOperand
+        self.result = result
+        self.left = left if left else ConstantOperand(np.int32(0))
+        self.right = right if right else ConstantOperand(np.int32(0))
 
     def output(self) -> Signals:
         raise NotImplementedError("")
@@ -42,8 +73,74 @@ class Operation(ASTNode):
                 self.operation == other.operation
                 and self.right == other.right
                 and self.left == other.left
+                and self.result == other.result
             )
         return False
 
     def __repr__(self) -> str:
         return f"Operation(operation={self.operation}, left={self.left}, right={self.right})"
+
+    @property
+    def left(self) -> Operand:
+        return self._left
+
+    @left.setter
+    def left(self, left: Operand) -> None:
+        if isinstance(left, SignalOperand):
+            if left.signal == Signal.SIGNAL_EACH:
+                if isinstance(self.right, SignalOperand):
+                    assert (
+                        self.right.signal != Signal.SIGNAL_EACH
+                    ), "Only one operand for Operation may be Each"
+            else:
+                assert (
+                    left.signal in ConcreteSignal
+                ), "Only concrete signals or Each are allowed in Operation"
+                assert (
+                    self.result.signal != Signal.SIGNAL_EACH
+                    or isinstance(self.right, SignalOperand)
+                    and self.right.signal == Signal.SIGNAL_EACH
+                ), "Can not assign signal other than each to Operation while having result each"
+        self._left = left
+
+    @property
+    def right(self) -> Operand:
+        return self._right
+
+    @right.setter
+    def right(self, right: Operand) -> None:
+        if isinstance(right, SignalOperand):
+            if right.signal == Signal.SIGNAL_EACH:
+                if isinstance(self.left, SignalOperand):
+                    assert (
+                        self.left.signal != Signal.SIGNAL_EACH
+                    ), "Only one operand for Operation may be Each"
+            else:
+                assert (
+                    right.signal in ConcreteSignal
+                ), "Only concrete signals or Each are allowed in Operation"
+            assert (
+                self.result.signal != Signal.SIGNAL_EACH
+                or isinstance(self.left, SignalOperand)
+                and self.left.signal == Signal.SIGNAL_EACH
+            ), "Can not assign signal other than each to Operation while having result each"
+        self._right = right
+
+    @property
+    def result(self) -> SignalOperand:
+        return self._result
+
+    @result.setter
+    def result(self, op: SignalOperand) -> None:
+        assert op.signal not in (
+            Signal.SIGNAL_EVERYTHING,
+            Signal.SIGNAL_ANYTHING,
+        ), "Anything or Everything are not allowed as Operation results"
+        if op.signal == Signal.SIGNAL_EACH:
+            assert (
+                isinstance(self.left, SignalOperand)
+                and self.left.signal == Signal.SIGNAL_EACH
+                or isinstance(self.right, SignalOperand)
+                and self.right.signal == Signal.SIGNAL_EACH
+            ), "Need to have one Operand as Each in order to set result to each"
+        self._result = op
--- test/AST/test_operation.py
+++ test/AST/test_operation.py
@@ -1,18 +1,22 @@
 import unittest
 
 from AST.Operation import *
+from AST.Network import Network
 
 
 class TestOperation(unittest.TestCase):
     def test_constructor(self):
-        op = Operation(NumericOperator.OR)
+        net = Network()
+        op = Operation(NumericOperator.OR, SignalOperand(net, Signal.SIGNAL_A))
         self.assertEqual(op.operation, NumericOperator.OR)
         self.assertEqual(op.left, ConstantOperand(np.int32(0)))
         self.assertEqual(op.right, ConstantOperand(np.int32(0)))
+        self.assertEqual(op.result, SignalOperand(net, Signal.SIGNAL_A))
 
     def test_eq(self):
-        op1 = Operation(NumericOperator.OR)
-        op2 = Operation(NumericOperator.ADD)
+        net = Network()
+        op1 = Operation(NumericOperator.OR, SignalOperand(net, Signal.SIGNAL_A))
+        op2 = Operation(NumericOperator.ADD, SignalOperand(net, Signal.SIGNAL_A))
         self.assertNotEqual(op1, ConstantOperand(np.int32(2)))
         self.assertNotEqual(op1, op2)
         op2.operation = NumericOperator.OR
@@ -22,4 +26,63 @@ class TestOperation(unittest.TestCase):
         op2.right = ConstantOperand(np.int32(50))
         self.assertNotEqual(op1, op2)
         op2.right = ConstantOperand(np.int32(0))
+        op1.result = SignalOperand(net, Signal.SIGNAL_B)
+        self.assertNotEqual(op1, op2)
+        op1.result = SignalOperand(net, Signal.SIGNAL_A)
         self.assertEqual(op1, op2)
+
+    def test_set_left(self):
+        net = Network()
+        op = Operation(NumericOperator.ADD, SignalOperand(net, Signal.SIGNAL_A))
+        op.right = SignalOperand(net, Signal.SIGNAL_EACH)
+        with self.assertRaises(AssertionError):
+            op.left = SignalOperand(net, Signal.SIGNAL_EACH)
+        with self.assertRaises(AssertionError):
+            op.left = SignalOperand(net, Signal.SIGNAL_ANYTHING)
+        with self.assertRaises(AssertionError):
+            op.left = SignalOperand(net, Signal.SIGNAL_EVERYTHING)
+        op.left = SignalOperand(net, Signal.SIGNAL_A)
+        op.right = SignalOperand(net, Signal.SIGNAL_A)
+        op.left = SignalOperand(net, Signal.SIGNAL_EACH)
+        op.right = ConstantOperand(np.int32(10))
+        op.left = SignalOperand(net, Signal.SIGNAL_EACH)
+        op.result = SignalOperand(net, Signal.SIGNAL_EACH)
+        with self.assertRaises(AssertionError):
+            op.left = SignalOperand(net, Signal.SIGNAL_A)
+
+    def test_right(self):
+        net = Network()
+        op = Operation(NumericOperator.ADD, SignalOperand(net, Signal.SIGNAL_A))
+        op.left = SignalOperand(net, Signal.SIGNAL_EACH)
+        with self.assertRaises(AssertionError):
+            op.right = SignalOperand(net, Signal.SIGNAL_EACH)
+        with self.assertRaises(AssertionError):
+            op.right = SignalOperand(net, Signal.SIGNAL_ANYTHING)
+        with self.assertRaises(AssertionError):
+            op.right = SignalOperand(net, Signal.SIGNAL_EVERYTHING)
+        op.right = SignalOperand(net, Signal.SIGNAL_A)
+        op.left = SignalOperand(net, Signal.SIGNAL_A)
+        op.right = SignalOperand(net, Signal.SIGNAL_EACH)
+        op.left = ConstantOperand(np.int32(10))
+        op.right = SignalOperand(net, Signal.SIGNAL_EACH)
+        op.result = SignalOperand(net, Signal.SIGNAL_EACH)
+        with self.assertRaises(AssertionError):
+            op.right = SignalOperand(net, Signal.SIGNAL_A)
+
+    def test_set_result(self):
+        net = Network()
+        op = Operation(NumericOperator.ADD, SignalOperand(net, Signal.SIGNAL_A))
+        with self.assertRaises(AssertionError):
+            op.result = SignalOperand(net, Signal.SIGNAL_EACH)
+        op.right = SignalOperand(net, Signal.SIGNAL_EACH)
+        op.result = SignalOperand(net, Signal.SIGNAL_EACH)
+        op.result = SignalOperand(net, Signal.SIGNAL_A)
+        op.right = SignalOperand(net, Signal.SIGNAL_A)
+        op.left = SignalOperand(net, Signal.SIGNAL_EACH)
+        op.result = SignalOperand(net, Signal.SIGNAL_EACH)
+        op.result = SignalOperand(net, Signal.SIGNAL_A)
+        op.left = SignalOperand(net, Signal.SIGNAL_A)
+        with self.assertRaises(AssertionError):
+            op.result = SignalOperand(net, Signal.SIGNAL_EVERYTHING)
+        with self.assertRaises(AssertionError):
+            op.result = SignalOperand(net, Signal.SIGNAL_ANYTHING)
