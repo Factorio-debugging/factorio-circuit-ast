eb098c9df1831bf0dac4ed6fbe37eb1587de2894
Fabian Wunsch <fabian@uriah.heep.sax.de>
2022-06-04 13:20:58 +0200

    added and tested DoubleNetwork
    for handling a red and a green network simultaneously as if it is one

---
+++ AST/DoubleNetwork.py
@@ -0,0 +1,66 @@
+from typing import Optional
+
+import numpy as np
+
+from .ASTNode import Signals
+from .Network import Network, NetworkType
+from .Signal import Signal, AbstractSignal
+
+
+class DoubleNetwork:
+    def __init__(
+        self, red: Optional[Network] = None, green: Optional[Network] = None
+    ) -> None:
+        self.red = red
+        self.green = green
+
+    def get_signal(self, signal: Signal) -> np.int32:
+        assert signal not in AbstractSignal
+        return (self.red.get_signal(signal) if self.red else np.int32(0)) + (
+            self.green.get_signal(signal) if self.green else np.int32(0)
+        )
+
+    def get_signals(self) -> Signals:
+        result: Signals = self.red.get_signals() if self.red else {}
+        if self.green:
+            for signal, value in self.green.get_signals().items():
+                result[signal] = result.setdefault(signal, np.int32(0)) + value
+        return result
+
+    def update_signal(self, signal: Signal, value: np.int32) -> None:
+        assert signal not in AbstractSignal
+        if self.red:
+            self.red.update_signal(signal, value)
+        if self.green:
+            self.green.update_signal(signal, value)
+
+    def update_signals(self, signals: Signals) -> None:
+        assert (
+            Signal.SIGNAL_EACH not in signals
+            and Signal.SIGNAL_EVERYTHING not in signals
+            and Signal.SIGNAL_ANYTHING not in signals
+        ), "only concrete signals are allowed in update_signals"
+        if self.red:
+            self.red.update_signals(signals)
+        if self.green:
+            self.green.update_signals(signals)
+
+    @property
+    def red(self) -> Optional[Network]:
+        return self._red
+
+    @red.setter
+    def red(self, net: Optional[Network]) -> None:
+        if net and net.type != NetworkType.RED:
+            raise TypeError("Can not assign green network to red DoubleNetwork part")
+        self._red = net
+
+    @property
+    def green(self) -> Optional[Network]:
+        return self._green
+
+    @green.setter
+    def green(self, net: Optional[Network]) -> None:
+        if net and net.type != NetworkType.GREEN:
+            raise TypeError("Can not assign green network to red DoubleNetwork part")
+        self._green = net
---
+++ test/AST/test_double_network.py
@@ -0,0 +1,125 @@
+import unittest
+
+from AST.DoubleNetwork import *
+
+
+class TestDoubleNetwork(unittest.TestCase):
+    def test_constructor(self):
+        d_net = DoubleNetwork()
+        self.assertIsNone(d_net.red)
+        self.assertIsNone(d_net.green)
+        r_net = Network(network=NetworkType.RED)
+        g_net = Network(network=NetworkType.GREEN)
+        d_net = DoubleNetwork(r_net, g_net)
+        self.assertIs(d_net.red, r_net)
+        self.assertIs(d_net.green, g_net)
+        with self.assertRaises(TypeError):
+            DoubleNetwork(red=g_net)
+        with self.assertRaises(TypeError):
+            DoubleNetwork(green=r_net)
+
+    def test_set_red(self):
+        d_net = DoubleNetwork()
+        with self.assertRaises(TypeError):
+            d_net.red = Network(network=NetworkType.GREEN)
+        r_net = Network(network=NetworkType.RED)
+        d_net.red = r_net
+        self.assertIs(d_net.red, r_net)
+        d_net.red = None
+        self.assertIsNone(d_net.red)
+
+    def test_set_green(self):
+        d_net = DoubleNetwork()
+        with self.assertRaises(TypeError):
+            d_net.green = Network(network=NetworkType.RED)
+        g_net = Network(network=NetworkType.GREEN)
+        d_net.green = g_net
+        self.assertIs(d_net.green, g_net)
+        d_net.green = None
+        self.assertIsNone(d_net.green)
+
+    def test_get_signal(self):
+        d_net = DoubleNetwork()
+        r_net = Network(network=NetworkType.RED)
+        g_net = Network(network=NetworkType.GREEN)
+        with self.assertRaises(AssertionError):
+            d_net.get_signal(Signal.SIGNAL_EACH)
+        self.assertEqual(d_net.get_signal(Signal.SIGNAL_A), 0)
+        d_net.red = r_net
+        d_net.green = g_net
+        self.assertEqual(d_net.get_signal(Signal.SIGNAL_A), 0)
+        r_net.update_signal(Signal.SIGNAL_A, np.int32(10))
+        g_net.update_signal(Signal.SIGNAL_A, np.int32(20))
+        g_net.update_signal(Signal.SIGNAL_B, np.int32(100))
+        r_net.tick()
+        g_net.tick()
+        self.assertEqual(d_net.get_signal(Signal.SIGNAL_A), 30)
+        self.assertIs(type(d_net.get_signal(Signal.SIGNAL_A)), np.int32)
+        self.assertEqual(d_net.get_signal(Signal.SIGNAL_B), 100)
+        self.assertIs(type(d_net.get_signal(Signal.SIGNAL_B)), np.int32)
+
+    def test_get_signals(self):
+        d_net = DoubleNetwork()
+        r_net = Network(network=NetworkType.RED)
+        g_net = Network(network=NetworkType.GREEN)
+        self.assertEqual(d_net.get_signals(), {})
+        d_net.red = r_net
+        d_net.green = g_net
+        self.assertEqual(d_net.get_signals(), {})
+        r_net.update_signal(Signal.SIGNAL_A, np.int32(10))
+        g_net.update_signal(Signal.SIGNAL_A, np.int32(20))
+        g_net.update_signal(Signal.SIGNAL_B, np.int32(100))
+        r_net.tick()
+        g_net.tick()
+        self.assertEqual(
+            d_net.get_signals(), {Signal.SIGNAL_A: 30, Signal.SIGNAL_B: 100}
+        )
+        self.assertTrue(
+            all(isinstance(i, np.int32) for i in d_net.get_signals().values())
+        )
+
+    def test_update_signal(self):
+        r_net = Network(network=NetworkType.RED)
+        g_net = Network(network=NetworkType.GREEN)
+        d_net = DoubleNetwork(r_net, g_net)
+        with self.assertRaises(AssertionError):
+            d_net.update_signal(Signal.SIGNAL_EACH, np.int32(10))
+        d_net.update_signal(Signal.ACCUMULATOR, np.int32(10))
+        r_net.tick()
+        g_net.tick()
+        self.assertEqual(r_net.get_signal(Signal.ACCUMULATOR), 10)
+        self.assertEqual(g_net.get_signal(Signal.ACCUMULATOR), 10)
+        d_net.red = None
+        d_net.update_signal(Signal.SIGNAL_A, np.int32(30))
+        g_net.tick()
+        self.assertEqual(g_net.get_signal(Signal.SIGNAL_A), 30)
+        d_net.red = r_net
+        d_net.green = None
+        d_net.update_signal(Signal.SIGNAL_A, np.int32(50))
+        r_net.tick()
+        self.assertEqual(r_net.get_signal(Signal.SIGNAL_A), 50)
+
+    def test_update_signals(self):
+        r_net = Network(network=NetworkType.RED)
+        g_net = Network(network=NetworkType.GREEN)
+        d_net = DoubleNetwork(r_net, g_net)
+        with self.assertRaises(AssertionError):
+            d_net.update_signals({Signal.SIGNAL_EACH: np.int32(10)})
+        signals: Signals = {
+            Signal.SIGNAL_A: 10,
+            Signal.SIGNAL_B: 30,
+        }
+        d_net.update_signals(signals)
+        r_net.tick()
+        g_net.tick()
+        self.assertEqual(r_net.get_signals(), signals)
+        self.assertEqual(g_net.get_signals(), signals)
+        d_net.red = None
+        d_net.update_signals(signals)
+        g_net.tick()
+        self.assertEqual(g_net.get_signals(), signals)
+        d_net.red = r_net
+        d_net.green = None
+        d_net.update_signals(signals)
+        r_net.tick()
+        self.assertEqual(r_net.get_signals(), signals)
